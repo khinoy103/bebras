50c3a3303d16dbc2f88c90b586767a0696b6a352
satriatna <satriatna212@gmail.com>
2021-05-08 13:10:04 +0700

    first commit

--- app/Http/Controllers/Admin/PhotoController.php
+++ app/Http/Controllers/Admin/PhotoController.php
@@ -1,84 +1,52 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Admin;
 
+use App\Category;
+use App\Event;
+use App\Http\Controllers\Controller;
+use App\Photo;
 use Illuminate\Http\Request;
 
 class PhotoController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
-        //
+        $photo = Photo::all();
+        return view('admin.photo.index', compact('photo'));
     }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
-        //
+        if($request->photo != null){
+            $photo = $request->file('photo');
+            $size = $photo->getSize();
+            $namePhoto = time() . "_" . $photo->getClientOriginalName();
+            $path = 'images';
+            $photo->move($path, $namePhoto);
+            $data['image'] =  $namePhoto;
+        }
+        $data['caption'] = $request->caption;
+        $photo = Photo::create($data);
+        return redirect()->back()->with('success','Data berhasil ditambahkan');
+    }
+    public function update(Request $request)
+    {
+        if($request->photo != null){
+            $photo = $request->file('photo');
+            $size = $photo->getSize();
+            $namePhoto = time() . "_" . $photo->getClientOriginalName();
+            $path = 'images';
+            $photo->move($path, $namePhoto);
+            $data['image'] =  $namePhoto;
+        }
+        $data['caption'] = $request->caption;
+        $photo = Photo::find($request->id)->update($data);
+        return redirect()->back()->with('success','Data berhasil diubah');
+    }
+    public function delete($id)
+    {
+        $photo = Photo::find($id)->delete();
+        return redirect()->back()->with('success','Data berhasil dihapus');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
 }
--- app/Post.php
+++ app/Post.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    protected $table='photos';
+    protected $table = 'posts';
     protected $guarded = [];
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
