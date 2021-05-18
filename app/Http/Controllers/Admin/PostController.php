181c74d5d04ea1ef3080e6efe86ebd2a3a314596
satriatna <satriatna212@gmail.com>
2021-05-18 11:20:26 +0700

    halo

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -26,13 +26,14 @@ class PostController extends Controller
         $namePhoto = time() . "_" . $photo->getClientOriginalName();
         $path = 'images';
         $photo->move($path, $namePhoto);
-
-        $data['image'] =  $namePhoto;
-        $data['title'] = $request->title;
-        $data['content'] = $request->content;
-        $data['category_id'] = $request->category_id;
-        $data['slug'] = $request->slug;
-        $post = Post::create($data);
+    
+        $post = Post::create([
+            'image' =>  $namePhoto,
+            'title' => $request->title,
+            'content' => $request->content,
+            'category_id' => $request->category_id,
+            'slug' => Str::slug($request->title, '-'),
+        ]);
         return redirect(route('admin.post.index'))->with('success','Data berhasil ditambahkan');
     }
     public function edit($id)
@@ -60,11 +61,12 @@ class PostController extends Controller
         }else{
             $data['image'] = $post->image;
         }
-        $data['title'] = $request->title;
-        $data['content'] = $request->content;
-        $data['slug'] = $request->slug;
-        $data['category_id'] = $request->category_id;
-        $post->update($data);
+        $post->update([
+            'title' => $request->title,
+            'content' => $request->content,
+            'slug' => Str::slug($request->title, '-'),
+            'category_id' => $request->category_id,
+        ]);
         return redirect(route('admin.post.index'))->with('success','Data berhasil diubah');
     }
     public function delete(Request $request, $id)
--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,7 @@ Auth::routes(['register' => false]);
 // Route::get('/home', 'HomeController@index')->name('home');
 
 Route::group(['middleware' => 'auth'], function () {
+    Route::get('home', 'HomeController@index');
     Route::prefix('admin')->name('admin.')->group(function () {    
         Route::group(['middleware' => 'admin'], function () {
             Route::get('/dashboard', 'Admin\DashboardController@index')->name('dashboard.index');
